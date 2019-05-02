0faf7a5924c20b15635dc3d5bbf004d2468503ac
akozyreva <anna.ko4523546@gmail.com>
2019-05-02 13:33:38 +0200

    add one more section

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <div class="row">
-      <Teaser/>
-      <div class="left-wrapper">
-        <ImgDiaper :index=isActive+1></ImgDiaper>
+    <Teaser/>
+    <section>
+      <div class="row">
+        <div class="left-wrapper">
+          <ImgDiaper :index=isActive+1></ImgDiaper>
+        </div>
+        <div class="right-wrapper">
+          <ChoosePack></ChoosePack>
+        </div>
       </div>
-      <div class="right-wrapper">
-        <ChoosePack></ChoosePack>
-      </div>
-    </div>
-
-<!--
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    </section>
+    <InfoFunction/>
   </div>
 </template>
 
@@ -21,13 +20,15 @@
 import ImgDiaper from './components/ImgDiaper.vue'
 import ChoosePack from './components/ChoosePack.vue'
 import Teaser from './components/Teaser.vue'
+import InfoFunction from './components/InfoFunction.vue'
 
 export default {
   name: 'app',
   components: {
     ImgDiaper,
     ChoosePack,
-    Teaser
+    Teaser,
+    InfoFunction
   }
 }
 </script>
@@ -37,17 +38,7 @@ export default {
 
 #app {
   font-family: "Cera-Regular",Arial,sans-serif;
-  /*-webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;*/
-  position: relative;
-  overflow: hidden;
-  margin: 0 auto;
-  padding: 2rem 0 2rem 0;
-  max-width: 80rem;
-  width: 100%;
+
 
   .left-wrapper{
     position: relative;
---
+++ src/components/InfoFunction.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+  <section class="dark">
+    <div class="row">
+      <h2 class="text-center">Wie es functioniert</h2>
+    </div>
+    <div class="wrapper-func">
+      <div class="wrapper-func-box" v-for="item in infos">
+        <img v-bind:src="imgUrl(item.imgName)" alt="">
+        <h4>{{ item.title }}</h4>
+        <p>{{ item.description }}</p>
+      </div>
+    </div>
+    <div class="row, text-center">
+      <a href="#" class="turquoise-color">Erfahre hier mehr über das <br />
+LILLYDOO Windel-Abo.</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      infos: [
+        {
+          title: 'DEIN TESTPAKET',
+          description: 'Wir schenken Dir die Produkte, Du übernimmst nur das Porto.',
+          imgName: 'lillydoo-your-trialbox-icon'
+        },
+        {
+          title: 'DEINE FLEXIBILITÄT',
+          description: 'Erste Monatsbox nach 14 Tagen für 49,50 € erhalten. Oder noch vor der ersten Lieferung kündigen.',
+          imgName: 'lillydoo-your-flexibility-icon'
+        },
+        {
+          title: 'DEINE MONATSBOX',
+          description: 'Jederzeit Designs oder Lieferintervalle anpassen oder die Windelgröße ändern.',
+          imgName: 'lillydoo-your-subscription-icon'
+        }
+      ]
+    }
+  },
+  methods: {
+    imgUrl(path) {
+      return require(`@/assets/img/${path}.svg`)
+   }
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper-func {
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  text-align: center;
+
+  .wrapper-func-box {
+    width: 33%;
+    padding: 1.5rem 0;
+    margin: 0 5rem
+  }
+}
+</style>

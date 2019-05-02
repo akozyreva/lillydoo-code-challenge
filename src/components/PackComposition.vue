917b8382ce02fd5d12b89a95deaa4cb7cd493203
akozyreva <anna.ko4523546@gmail.com>
2019-05-02 13:39:49 +0200

    add img, add one more component

---
+++ src/components/PackComposition.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <section>
+    <div class="row">
+      <h2 class="text-center">Dein Testpaket enthält</h2>
+    </div>
+    <div class="row">
+      <div class="wrapper-pack-compose">
+        <h4>10 LILLYDOO WINDELN</h4>
+        <ul>
+          <li>0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz</li>
+          <li>0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>

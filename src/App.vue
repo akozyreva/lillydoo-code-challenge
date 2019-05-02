e14a3c99bc6c5ebee90d9c2cc476d6db59773d00
akozyreva <anna.ko4523546@gmail.com>
2019-05-02 11:59:22 +0200

    add fixes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
-    <section class="trialbox-teaser">
-      <div class="row wrapper-teaser-button">
-        <h1>Entdecke unsere Hautfreundlichkeit</h1>
-        <div class="wrapper-button">
-          <a href="#" class="button">Jetzt Deine Größe wählen</a>
-        </div>
-      </div>
-    </section>
     <div class="row">
+      <Teaser/>
       <div class="left-wrapper">
         <ImgDiaper :index=isActive+1></ImgDiaper>
       </div>
@@ -27,12 +20,14 @@
 
 import ImgDiaper from './components/ImgDiaper.vue'
 import ChoosePack from './components/ChoosePack.vue'
+import Teaser from './components/Teaser.vue'
 
 export default {
   name: 'app',
   components: {
     ImgDiaper,
-    ChoosePack
+    ChoosePack,
+    Teaser
   }
 }
 </script>
@@ -67,25 +62,5 @@ export default {
 
   }
 
-  .trialbox-teaser {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    background-size: 100%;
-    max-width: 100%;
-    height: 37vw;
-    min-height: 300px;
-    max-height: 550px;
-    background-repeat: no-repeat;
-    background-image: url(./assets/img/lillydoo-testpaket-header-large.jpg);
-  }
-
-  .wrapper-teaser-button {
-    float: left;
-    position: relative;
-    max-width: 22rem;
-    margin-left: 8rem;
-    background-repeat: no-repeat;
-  }
 }
 </style>
---
+++ src/components/Teaser.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <section class="trialbox-teaser">
+    <div class="row wrapper-teaser-button">
+      <h1>Entdecke unsere Hautfreundlichkeit</h1>
+      <div class="wrapper-button">
+        <a href="#" class="button">Jetzt Deine Größe wählen</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.trialbox-teaser {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-size: 100%;
+  max-width: 100%;
+  height: 37vw;
+  min-height: 300px;
+  max-height: 550px;
+  background-repeat: no-repeat;
+  background-image: url(~@/assets/img/lillydoo-testpaket-header-large.jpg);
+}
+
+.wrapper-teaser-button {
+  float: left;
+  position: relative;
+  max-width: 22rem;
+  margin-left: 8rem;
+  background-repeat: no-repeat;
+}
+</style>
